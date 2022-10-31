8844339ebe76d06f5dc37bd58662ad4fa7d9d07f
Prophet Lamb <prophet.lamb@gmail.com>
2022-10-31 17:53:13 +0100

    Add Span.ClipLength clips the length of a span the the maximum length

--- src/Common/MemoryExtensions.cs
+++ src/Common/MemoryExtensions.cs
@@ -1,7 +1,10 @@
 namespace SurrealDB.Common;
 
 internal static class MemoryExtensions {
-    public static ReadOnlySpan<T> SliceToMin<T>(in this ReadOnlySpan<T> span, int length) {
+    public static Span<T> ClipLength<T>(in this Span<T> span, int length) {
+        return span.Length <= length ? span : span.Slice(0, length);
+    }
+    public static ReadOnlySpan<T> ClipLength<T>(in this ReadOnlySpan<T> span, int length) {
         return span.Length <= length ? span : span.Slice(0, length);
     }
 }
