0003150fb622cf89b82f251926e9a8dab6eb0ee8
Prophet Lamb <prophet.lamb@gmail.com>
2022-10-31 15:58:13 +0100

    Add Task.Inv == Task.ConfigureAwait(false)

---
+++ src/Common/TaskExtensions.cs
@@ -0,0 +1,22 @@
+using System.Runtime.CompilerServices;
+
+namespace SurrealDB.Common;
+
+/// <summary>Extension methods for Tasks</summary>
+public static class TaskExtensions {
+    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
+    /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
+    public static ConfiguredTaskAwaitable Inv(this Task t) => t.ConfigureAwait(false);
+
+    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
+    /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
+    public static ConfiguredTaskAwaitable<T> Inv<T>(this Task<T> t) => t.ConfigureAwait(false);
+
+    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
+    /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
+    public static ConfiguredValueTaskAwaitable Inv(this ValueTask t) => t.ConfigureAwait(false);
+
+    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
+    /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
+    public static ConfiguredValueTaskAwaitable<T> Inv<T>(this ValueTask<T> t) => t.ConfigureAwait(false);
+}
