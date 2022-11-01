eb49c68338714147b91d9549fdb3ade0c4833723
Prophet Lamb <prophet.lamb@gmail.com>
2022-11-01 15:47:38 +0100

    Docs

--- src/Common/TaskExtensions.cs
+++ src/Common/TaskExtensions.cs
@@ -4,19 +4,19 @@ namespace SurrealDB.Common;
 
 /// <summary>Extension methods for Tasks</summary>
 public static class TaskExtensions {
-    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
+    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.
+    /// The previous context is not restored upon completion.</summary>
     /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ConfiguredTaskAwaitable Inv(this Task t) => t.ConfigureAwait(false);
 
-    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
-    /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
+    /// <inheritdoc cref="Inv(Task)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ConfiguredTaskAwaitable<T> Inv<T>(this Task<T> t) => t.ConfigureAwait(false);
-
-    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
-    /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
-    public static ConfiguredValueTaskAwaitable Inv(this ValueTask t) => t.ConfigureAwait(false);
-
-    /// <summary>The task is <see cref="SynchronizationContext"/> invariant.</summary>
-    /// <remarks>Equivalent to <code>Task.ConfigureAwait(false)</code>.</remarks>
-    public static ConfiguredValueTaskAwaitable<T> Inv<T>(this ValueTask<T> t) => t.ConfigureAwait(false);
+    /// <inheritdoc cref="Inv(Task)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ConfiguredValueTaskAwaitable Inv(in this ValueTask t) => t.ConfigureAwait(false);
+    /// <inheritdoc cref="Inv(Task)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ConfiguredValueTaskAwaitable<T> Inv<T>(in this ValueTask<T> t) => t.ConfigureAwait(false);
 }
